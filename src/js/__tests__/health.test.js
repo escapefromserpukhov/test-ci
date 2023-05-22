225706267a4d94b2e95871c6aefaf44c2cef8476
Viktoria_finch <85229737+escapefromserpukhov@users.noreply.github.com>
2023-05-22 09:56:40 +0300

    Add files via upload

---
+++ src/js/__tests__/health.test.js
@@ -0,0 +1,49 @@
+import showHealth from '../health';
+
+test('should healthy', () => {
+  const received = showHealth({ name: 'Маг', health: 90 });
+  expect(received).toBe('healthy');
+});
+
+test('should wounded', () => {
+  const received = showHealth({ name: 'Маг', health: 40 });
+  expect(received).toBe('wounded');
+});
+
+test('should critical', () => {
+  const received = showHealth({ name: 'Маг', health: 10 });
+  expect(received).toBe('critical');
+});
+
+test('Parameter is not a number!', () => {
+  expect(() => {
+    showHealth({});
+  }).toThrow();
+});
+
+test('Parameter is not a number!', () => {
+  expect(() => {
+    showHealth({ name: 'Маг', health: 'mistake' });
+  }).toThrow();
+});
+
+test('Parameter is not a number!', () => {
+  expect(() => {
+    showHealth({ name: 'Маг', health: -10 });
+  }).toThrow();
+});
+
+/* test('should killed', () => {
+  const received = showHealth({ name: 'Маг', health: 'mistake' });
+  expect(received).toBe('killed');
+});
+
+test('should killed', () => {
+  const received = showHealth({});
+  expect(received).toBe('killed');
+});
+
+test('should killed', () => {
+  const received = showHealth({ name: 'Маг', health: -10 });
+  expect(received).toBe('killed');
+}); */
